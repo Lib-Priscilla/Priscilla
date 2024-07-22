3e5f17cbab7f16b7221f2d4d7f05007970165abd
cpu governor <cpu.governor@gmail.com>
2024-07-23 00:41:05 +0100

    rebase program

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_ttf.h>
+#include<SDL2/SDL_image.h>
+#include"sdl_template.h"
+#include"priscilla.h"
+
+int main (){
+	create_window("GUI",720,360);
+	
+	terminate_window();
+	return 0;
+}
---
+++ src/priscilla.h
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_ttf.h>
+#include<SDL2/SDL_image.h>
+#include"sdl_template.h"
+
+
+bool create_window(const char *title,int width, int height){
+	init(title,width,height);
+}
+
+void terminate_window(){
+	quit();
+}
+void window_background(Color color){
+	clear_screen(color);
+}
+void button (Color color,int w,int h,int x,int y){
+	draw_rectangle(color,w,h,x,y,FILLED);
+}
